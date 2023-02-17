da3d32ebdd195fea525496d8511a6a3723b86326
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-02-17 15:19:49 -0800

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,21 +2,21 @@
 
 /**
  * main - prints all possible combinations of single-digit numbers
- * Return: Always 0
+ * Return: Always 0 (Succesful)
  */
 int main(void)
 {
-	int n;
+	int a;
 
-	for (n = 48; n < 58; n++)
-
-		putchar(n);
-		if (n != 57)
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a + '0');
+		if (a < 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
-	putchar('\n')
-
+	}
+	putchar('\n');
 	return (0);
 

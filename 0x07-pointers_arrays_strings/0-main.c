241a807eefe267fd04a212bdc963b68eca890b28
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-03-06 04:06:53 -0800

    0-main.c

--- 0x07-pointers_arrays_strings/0-main.c
+++ 0x07-pointers_arrays_strings/0-main.c
@@ -13,13 +13,12 @@ void simple_print_buffer(char *buffer, unsigned int size)
 	unsigned int i;
 
 	i = 0;
-
 	while (i < size)
 	{
 	if (i % 10)
 	{
-	printf(" ");
-	{
+		printf(" ");
+	}
 	if (!(i % 10) && i)
 	{
 		printf("\n");

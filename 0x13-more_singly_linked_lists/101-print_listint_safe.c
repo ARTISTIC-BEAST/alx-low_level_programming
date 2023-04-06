e30321d76717d833fb131b5923d7dd9768d418d9
ARTISTIC-BEAST <artisticbeast1@gmail.com>
2023-04-05 19:52:53 -0700

    101-print_listint_safe.c

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,28 +10,29 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp;
-	size_t i, j;
-	const listint_t *array[100];
+	int nodes = 0;
+	const listint_t *kichwa = head;
 
 	if (head == NULL)
-	exit(98);
-
-	for (i = 0; head != NULL; i++)
 	{
-	for (j = 0; j < i; j++)
+		return (0);
+	}
+	printf("[%p] %i\n", (void *)head, (*head).n);
+	head = (*head).next;
+	for (nodes = 0; head != NULL; nodes++)
 	{
-	if (head == array[j])
-	{
-	printf("-> [%p] %d\n", (void *)head, head->n);
-	return (i);
+		if (head < kichwa)
+		{
+			printf("[%p] %i\n", (void *)head, (*head).n);
+		}
+		else
+		{
+			printf("-> [%p] %i\n", (void *)head, (*head).n);
+			break;
+		}
+		head = (*head).next;
+		kichwa = (*kichwa).next;
 	}
-	}
-	array[i] = head;
-	temp = head;
-	head = head->next;
-	printf("[%p] %d\n", (void *)temp, temp->n);
-	}
-	return (i);
+	return (nodes + 1);
 }
 
